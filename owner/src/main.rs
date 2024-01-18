8e961eb6bcc821ca7d2fdf1f43efc0f420d70802
astoria <2765036345@qq.com>
2024-01-18 23:03:12 +0800

    ADD.

--- owner/src/main.rs
+++ owner/src/main.rs
@@ -1,16 +1,17 @@
+
 fn main() {
-    let s1 = get_string();
-
-    let s2 = String::from("yours");
-
-    let s3 = take_and_give_back(s2);
-
+    let str = String::from("this is a demo string");
+    let index = first_word(&str);
+    str.clear();
+    println!("{index}");
 }
 
-fn get_string()->String{
-    String::from("hello world")
-}
-
-fn take_and_give_back(a_string:String)->String{
-    a_string
+fn first_word(str:&String)->&str{
+    let bytes = str.as_bytes();
+    for (i,&item) in bytes.iter().enumerate(){
+        if item == b' ' {
+            return &str[..i];
+        }
+    }
+    &str[..]
 }
